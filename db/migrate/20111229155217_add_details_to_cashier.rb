578e0f3d49daa54f8acff4826cebbe40794ed934
John Maenard V. Elauria <johnelauria@hotmail.com>
2013-02-05 23:10:08 +0800

    sdkjfkjsh

--- db/migrate/20111229155217_add_details_to_cashier.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToCashier < ActiveRecord::Migration
-  def change
-    add_column :cashiers, :password_salt, :string
-    add_column :cashiers, :persistence_token, :string
-    add_column :cashiers, :name, :string
-    add_column :cashiers, :staffnum, :string
-  end
-end
